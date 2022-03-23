31887216dca55665c9fc0496eea374eff2e67459
Ola Fivelstad Smaaberg <olafsm@stud.ntnu.no>
2022-03-23 14:42:26 +0100

    added SMBus to raspberryPI

--- src/mqtt_raspberrypi.py
+++ src/mqtt_raspberrypi.py
@@ -4,9 +4,17 @@ from smbus import SMBus
 
 bus = SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
-SLAVE_ADDRESS = 0x04      #7 bit address (will be left shifted to add the read write bit)
+SLAVE_ADDRESS = 0x04
 
-#Write a single register
+
+def translate_range(v, from_min,from_max, to_min, to_max):
+    from_span = from_max - from_min
+    to_span = to_max - to_min
+
+    valueScaled = float(v - from_min) / float(from_span)
+
+    return to_min + (valueScaled * to_span)
+
 
 class messager:
     def __init__(self):
@@ -15,7 +23,11 @@ class messager:
         self.gas = 0
         self.steer = 0
     def on_message(self, client, userdata, message):
-        t = int(message.payload.decode("utf-8"))
+        try:
+            t = int(message.payload.decode("utf-8"))
+        except Exception as E:
+            print("not integer")
+
         if(message.topic == "gas"):
             self.gc += 1
             self.gas += (time.time_ns() - t)/1000000
@@ -37,13 +49,20 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, message):
-    t = int(message.payload.decode("utf-8"))
-    t_d = (time.time_ns() - t)/1000000
+    try:
+        t = int(message.payload.decode("utf-8"))
+    except Exception as E:
+        print("not integer")
+        return
+    #t_d = (time.time_ns() - t)/1000000
     if(message.topic == "gas"):
+        bus.write_byte_data(SLAVE_ADDRESS, translate_range(t, 0,1,0,127))
         print("gas:", t_d)
 
     if(message.topic == "steer"):
         print("steer:", t_d)
+        bus.write_byte_data(SLAVE_ADDRESS, translate_range(t, 0,1,128,255))
+
 
 
 def on_message3(client, userdata, message):
